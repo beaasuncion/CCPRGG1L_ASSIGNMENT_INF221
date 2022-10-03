877f5baf4d4ad68e972584f7cfba09f6d5240e81
Beatriz Asuncion <111472853+beaasuncion@users.noreply.github.com>
2022-10-03 11:32:58 +0800

    Create Assignment2.java
    Here's my assignment 2

---
+++ Assignment2.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 5; i != 0; i--) {
+         
+         if (i == 4) {
+                System.out.println("WAIT! Need a break!");
+                break;
+            }
+        
+            System.out.println("Beatriz");
+        
+        }
+        int surnameCount= 5;
+
+        do {
+            System.out.println("Asuncion");
+            surnameCount--;
+        } while (surnameCount != 0);
+        
+        int nicknameCount = 5;
+          
+        while (nicknameCount != 0) {
+            
+            nicknameCount--;
+            if (nicknameCount == 2) {
+                System.out.println("SKIP THIS GO TO THE NEXT LOOP");
+                continue;
+            }
+            
+            System.out.println("Beyaaaboo"); 
+            
+        }
+    }
+}
